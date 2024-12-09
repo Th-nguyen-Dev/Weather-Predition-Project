af35115efa063ebb1432bd8dfc6be5c29a9c942e
Ryan Nguyen <th.nguyen.developer@gmail.com>
2024-12-09 02:39:40 -0800

    Refactor and new data entries

--- getloc.sh
+++ getloc.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Directory containing the files
-DIR="draft-final-data"
+DIR="processed-final-data-2"
+# Directory to save the downloaded files
+SAVE_DIR="./location-data"
+
+# Create the save directory if it doesn't exist
+mkdir -p "$SAVE_DIR"
 
 # Loop through each file in the directory
 for FILE in "$DIR"/*; do
     # Extract the file name from the path
     FILENAME=$(basename "$FILE")
     
-    # Execute the wget command
-    wget -O "$FILENAME" https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/2024/"$FILENAME"
+    # Execute the wget command and save the file in the save directory
+    wget -O "$SAVE_DIR/$FILENAME" https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/2024/"$FILENAME"
 done
